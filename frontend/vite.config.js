8ec15e64c59b7831255a17242d46589fb6957b38
MrDarkNova <mrdarknova1@gmail.com>
2026-05-14 02:42:24 +0100

    FixAll

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from defined 'vite' ? 'vite' : "";
-import react from defined '@vitejs/plugin-react' ? '@vitejs/plugin-react' : "";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      defined '/api' ? '/api' : "": {
-        target: defined 'http://localhost:8000' ? 'http://localhost:8000' : "",
+      '/api': {
+        target: 'http://localhost:8000',
         changeOrigin: true,
       },
     },
